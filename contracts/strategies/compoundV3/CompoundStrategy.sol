5e7e63c99229a74d4a0b70e32ddef82782b37097
CryptJS13 <cryptjs13@protonmail.com>
2025-05-16 13:25:35 +0200

    Update compound

--- contracts/strategies/compoundV3/CompoundStrategy.sol
+++ contracts/strategies/compoundV3/CompoundStrategy.sol
@@ -89,15 +89,13 @@ contract CompoundStrategy is BaseUpgradeableStrategy {
   function _handleFee() internal {
     _accrueFee();
     uint256 fee = pendingFee();
-    if (fee > 100) {
-      uint256 balanceIncrease = fee.mul(feeDenominator()).div(totalFeeNumerator());
+    if (fee > 1e4) {
       _withdrawUnderlyingFromPool(fee);
       address _underlying = underlying();
-      if (IERC20(_underlying).balanceOf(address(this)) < fee) {
-        balanceIncrease = IERC20(_underlying).balanceOf(address(this)).mul(feeDenominator()).div(totalFeeNumerator());
-      }
+      fee = Math.min(fee, IERC20(_underlying).balanceOf(address(this)));
+      uint256 balanceIncrease = fee.mul(feeDenominator()).div(totalFeeNumerator());
       _notifyProfitInRewardToken(_underlying, balanceIncrease);
-      setUint256(_PENDING_FEE_SLOT, 0);
+      setUint256(_PENDING_FEE_SLOT, pendingFee().sub(fee));
     }
   }
 
@@ -204,7 +202,7 @@ contract CompoundStrategy is BaseUpgradeableStrategy {
     _handleFee();
     _claimReward();
     _liquidateReward();
-    _withdrawUnderlyingFromPool(currentSupplied().sub(pendingFee().add(1)));
+    _withdrawUnderlyingFromPool(currentSupplied().sub(pendingFee()));
     address underlying_ = underlying();
     IERC20(underlying_).safeTransfer(vault(), IERC20(underlying_).balanceOf(address(this)));
     _updateStoredSupplied();
